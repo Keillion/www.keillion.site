075e93242c55556ae950ff27878ceadadad0083f
Keillion <735699921@qq.com>
2020-05-08 13:17:33 +0800

    Update kUtil.js

--- js/kUtil.js
+++ js/kUtil.js
@@ -91,13 +91,13 @@ Date.prototype.kUtilFormat = function(fmt){
         "m+" : this.getUTCMinutes(),
         "s+" : this.getUTCSeconds(),
         "q+" : Math.floor((this.getUTCMonth()+3)/3),
-        "S"  : this.getUTCMilliseconds()
+        "S+"  : this.getUTCMilliseconds()
     };
     if(/(y+)/.test(fmt))
         fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
     for(var k in o)
         if(new RegExp("("+ k +")").test(fmt))
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("000"+ o[k]).substr(("000"+ o[k]).length - RegExp.$1.length)));
     return fmt;
 };
 kUtil.getTimeStampFromStr = function(yyyyMMddHHmmss, timeZone){// or yyyyMMdd
